1c731062aeba6b2ffc41407cbe455e5b7e7f8786
rajanjamu <rajanjamu@gmail.com>
2020-04-20 16:34:05 +0530

    app.js

--- client/main.js
+++ client/main.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return(
-    <div>
-      <h1>This is a MarkBin project</h1>
-    </div>
-  );
-}
+import App from './components/app';
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.render-target'));
